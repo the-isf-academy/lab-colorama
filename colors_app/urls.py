2386b0326603980d86e6be4736ade5bafade5333
Chris Proctor <chris@chrisproctor.net>
2021-03-21 22:40:10 -0400

    Added code from lesson 1 completion

--- colors_app/urls.py
+++ colors_app/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from colors_app import views
+from colors_app.class_based_views import NewColorView, ColorListView
 
 app_name = "colors_app"
 urlpatterns = [
     path('', views.home_view, name="index"),
     path('colors/random', views.random_color_view, name="random_color"),
+    path('colors/new', NewColorView.as_view(), name='new_color'),
+    path('colors', ColorListView.as_view(), name='color_list'),
 ]
 
